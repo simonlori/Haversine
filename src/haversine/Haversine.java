8f83358a113fd468453312a9c81781ea399136b1
simonlori <ss_lori@yahoo.com>
2020-07-19 16:39:10 +0300

    harmadik

--- src/haversine/Haversine.java
+++ src/haversine/Haversine.java
@@ -5,6 +5,15 @@
  */
 package haversine;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author Lori
@@ -18,29 +27,73 @@ public class Haversine {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        double lat01 = 46.186666670721024;
-        double lon01 = 24.586666646879166;
-        double lat02 = 46.186388892820105;
-        double lon02 = 24.586388868978247;
-        double a = getDistance(lat01, lat02, lon01, lon02);
-        System.out.println("kilometer:");
-        System.out.println(a);
-        double b = a * 1000;
-        System.out.println("meter:");
-        System.out.println(b);
+        /*double lat01 = 46.186666670721024;
+         double lon01 = 24.586666646879166;
+         double lat02 = 46.186388892820105;
+         double lon02 = 24.586388868978247;
+         double a = getDistance(lat01, lat02, lon01, lon02);
+         System.out.println("kilometer:");
+         System.out.println(a);
+         double b = a * 1000;
+         System.out.println("meter:");
+         System.out.println(b);*/
 
-        double[] tomb1 = new double[10];
-        for (int i = 0; i < tomb1.length; i++) {
-            tomb1[i] = 1;
+        /*
+         try {
+         File myObj = new File("koordinatak.txt");
+         Scanner myReader = new Scanner(myObj);
+         while (myReader.hasNextLine()) {
+         String data = myReader.nextLine();
+         data = data.replace(',', ' ');
+         System.out.println(data);
+         }
+         myReader.close();
+         } catch (FileNotFoundException e) {
+         System.out.println("An error occurred.");
+         e.printStackTrace();
+         }
+         */
+        ArrayList<Double> lista = new ArrayList<Double>();
+        ArrayList<Double> lat = new ArrayList<Double>();
+        ArrayList<Double> lon = new ArrayList<Double>();
+        
+        ArrayList<Double> lista2 = new ArrayList<Double>();
+        ArrayList<Double> lat2 = new ArrayList<Double>();
+        ArrayList<Double> lon2 = new ArrayList<Double>();
+        
+        try {
+            File myObj = new File("koordinatak.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNext()) {
+                String d = myReader.next();
+                double a = Double.parseDouble(d);
+                //System.out.println(a);
+                lista.add(a);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
 
-        /*double eredmeny,perc,mperc;
-         perc = (float) 35/60;
-         System.out.println(perc);
-         mperc = (float) 49.28/3600;
-         System.out.println(mperc);
-         eredmeny = 24 + perc + mperc;
-         System.out.println(eredmeny);*/
+        int k = 0;
+        for (int i = 0; i < lista.size(); i++) {
+            //System.out.println(lista.get(i));
+            ++k;
+        }
+        //System.out.println(k);
+
+        for (int i = 0; i < lista.size(); i += 3) {
+            //System.out.println(lista.get(i));
+            lat.add(lista.get(i));
+        }
+
+        for (int i = 1; i < lista.size(); i += 3) {
+            //
+            System.out.println(lista.get(i));
+            lon.add(lista.get(i));
+        }
+
     }
 
     public static double getDistance(double lat1, double lat2, double lon1, double lon2) {
--- src/haversine/NewJFrame.java
+++ src/haversine/NewJFrame.java
@@ -589,20 +589,6 @@ public class NewJFrame extends javax.swing.JFrame {
         });
     }
 
-    /*public static double getDistance(double lat1, double lat2, double lon1, double lon2){
-     double dLon = Math.toRadians(lon2-lon1);
-     double dLat = Math.toRadians(lat2-lat1);
- 
-     double a = (Math.sin(dLat / 2) * Math.sin(dLat / 2)) +
-     (Math.cos(Math.toRadians(lat1))) *
-     (Math.cos(Math.toRadians(lat2))) *
-     (Math.sin(dLon / 2)) *
-     (Math.sin(dLon / 2));
-     double angle = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-     double result = angle * R;
-     return result;
-     }*/
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
